a3700d27061cc5aa813e62fc89a0683f96a1cfec
kopa <pablosko03@gmail.com>
2025-05-05 22:22:30 +0200

    cambios importantes en el contact.js para añadir el patch y actualizar contactos

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,86 @@
 const baseUrl = "https://irileofdjkcmspvebnqq.supabase.co"
 const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyaWxlb2ZkamtjbXNwdmVibnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNzk1MDYsImV4cCI6MjA0OTg1NTUwNn0.MZx5Cpcw6aqM7A9Sc8_VC6HWnSKQ0SYkWpTqUAI0-Pg"
 
-function createContact() {
+let params = new URLSearchParams(window.location.search);
+let idEdit = params.get("id");
+
+if (idEdit) {
+    document.getElementById("title").innerText = "Editar contacto";
+    document.getElementById("save").innerText = "Actualizar contacto";
+    document.getElementById("save").classList.remove("hidden");
+    document.getElementById("create").classList.add("hidden");
+
+    getContact();
+    document.getElementById("save").addEventListener("click", updateContact);
+
+}else{
+    document.getElementById("create").addEventListener("click", createContact);
+}
+
+function getContact() {
+    const userId = localStorage.getItem("userId")
+    const accessToken = localStorage.getItem("token")
+
+    let url = baseUrl + "/rest/v1/contacts"
+    url += `?user_id=eq.${userId}`
+    url += `&id=eq.${idEdit}`
+
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "apiKey": apiKey,
+            "Authorization": `Bearer ${accessToken}`
+        }
+    }
+
+    fetch(url, requestOptions)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("La API ha respondido: ", data)
+            document.getElementById("name").value = data[0].name
+            document.getElementById("email").value = data[0].email
+            document.getElementById("phone").value = data[0].phone
+        })
+        .catch((error) => {
+            console.log("Ha ocurrido un error: ", error)
+        })
+}
+
+function updateContact() {
+    const userId = localStorage.getItem("userId")
+    const accessToken = localStorage.getItem("token")
+
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+    const phone = document.getElementById("phone").value
+
+    const requestOptions = {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "apiKey": apiKey,
+            "Authorization": `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            phone: phone,
+            user_id: userId
+        })
+    }
+
+    const url = baseUrl + "/rest/v1/contacts?id=eq." + idEdit
+
+    fetch(url, requestOptions)
+        .then((response) => {
+            if (response.status == 204) {
+                window.location.href = "index.html"
+            }
+        })
+        .catch((error) => console.error(error));
+}
+
+async function createContact() {
     const user_id = localStorage.getItem("userId")
     const access_token = localStorage.getItem("token")
 
--- js/main.js
+++ js/main.js
@@ -33,14 +33,49 @@ function loadContacts() {
 function printData(data) {
     console.log("estas en print data: ",data)
     const dataList = document.getElementById("list")
+    dataList.innerHTML = "";
 
     data.forEach(contact => {
-        dataList.innerHTML += `<div class="bg-white shadow rounded-xl px-4 py-2">
-            <h1 class="font-bold text-lg text-blue-800">${contact.name}</h1>
-            <p class="px-2 py-0.5 bg-indigo-700 w-fit rounded-full text-white">${contact.email}</p>
-            <p class="text-slate-700">${contact.phone}</p>
+        dataList.innerHTML += `<div class="bg-white shadow rounded-xl px-4 py-2 flex justify-between">
+            <div>
+                <h1 class="font-bold text-lg text-blue-800">${contact.name}</h1>
+                <p class="px-2 py-0.5 bg-indigo-700 w-fit rounded-full text-white">${contact.email}</p>
+                <p class="text-slate-700">${contact.phone}</p>
+            </div>
+            <div>
+                <button onclick="deleteContact(${contact.id})" class="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700 transition-all">
+                    Eliminar contacto
+                </button>
+
+                <a href="new-contact.html?id=${contact.id}" class="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-700 transition-all">
+                    Editar contacto
+                </a>
+            </div>
         </div>`
     });
+}
+
+function deleteContact(id) {
+    const accessToken = localStorage.getItem("token")
+    const url = baseUrl + "/rest/v1/contacts?id=eq." + id
+
+    const requestOptions = {
+        method: "DELETE",
+        headers: {
+            "apiKey": apiKey,
+            "Authorization": `Bearer ${accessToken}`
+        }
+    }
+
+    fetch(url, requestOptions)
+        .then((res) => {
+            if (res.status == 204) {
+                loadContacts();
+            }
+        })
+        .catch((error) => {
+            console.log("Ha ocurrido un error: ", error)
+        })
 }
 
 loadContacts()
